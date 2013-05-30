0bf1ec2094053b45e6fad157909ca09e0db3208b
Alexander Muzychenko <alexmuz@gmail.com>
2013-05-30 21:13:40 +0400

    bug fix in theme nifty

--- themes/nifty/nifty.shop.js
+++ themes/nifty/nifty.shop.js
@@ -5,22 +5,12 @@ $(document).ready(function () {
         $.post(f.attr('action'), f.serialize(), function (response) {
             if (response.status == 'ok') {
                 var cart_total = $(".cart-total");
-                
-                if ($("div.cart").length) {
-                    $(".content").parent().load(location.href, function () {
-                        cart_total.closest('#cart').removeClass('empty');
-                        cart_total.html(response.data.total);    
-                    });
-                } else {
-                
-                    cart_total.closest('#cart').removeClass('empty');
-                    cart_total.html(response.data.total);
-                    
-                    f.find('input[type="submit"]').hide();
-                    f.find('.price').hide();
-                    f.find('span.added2cart').show();
-                    
-                }
+                cart_total.closest('#cart').removeClass('empty');
+                cart_total.html(response.data.total);
+
+                f.find('input[type="submit"]').hide();
+                f.find('.price').hide();
+                f.find('span.added2cart').show();
             } else if (response.status == 'fail') {
                 alert(response.errors);
             }
