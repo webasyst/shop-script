983684d3e599334f2f45a22f17d7c363ff0b461b
Alexander Muzychenko <alexmuz@gmail.com>
2013-12-20 16:04:09 +0400

    theme Facebook

--- themes/facebook/facebook.shop.js
+++ themes/facebook/facebook.shop.js
@@ -2,8 +2,33 @@ $(document).ready(function () {
 
     $('.bxslider').bxSlider( { auto : false, pause : 5000, autoHover : true });
 
-    $(".product-list").on('submit', 'form.addtocart', function () {
+    $('.dialog').on('click', 'a.dialog-close', function () {
+        $(this).closest('.dialog').hide().find('.cart').empty();
+        return false;
+    });
+
+    $(document).keyup(function(e) {
+        if (e.keyCode == 27) {
+            $(".dialog:visible").hide().find('.cart').empty();
+        }
+    });
+
+    $(".content").on('submit', '.product-list form.addtocart', function () {
         var f = $(this);
+        if (f.data('url')) {
+            var d = $('#dialog');
+            var c = d.find('.cart');
+            c.load(f.data('url'), function () {
+                c.prepend('<a href="#" class="dialog-close">&times;</a>');
+                d.show();
+                if ((c.height() > c.find('form').height())) {
+                    c.css('bottom', 'auto');
+                } else {
+                    c.css('bottom', '15%');
+                }
+            });
+            return false;
+        }
         $.post(f.attr('action'), f.serialize(), function (response) {
             if (response.status == 'ok') {
                 var cart_total = $(".cart-total");
@@ -21,5 +46,17 @@ $(document).ready(function () {
         }, "json");
         return false;
     });
-
+    $('.filters.ajax form input').change(function () {
+        var f = $(this).closest('form');
+        var url = '?' + f.serialize();
+        $(window).lazyLoad && $(window).lazyLoad('sleep');
+        $.get(url, function(html) {
+            var tmp = $('<div></div>').html(html);
+            $('#product-list').html(tmp.find('#product-list').html());
+            if (!!(history.pushState && history.state !== undefined)) {
+                window.history.pushState({}, '', url);
+            }
+            $(window).lazyLoad && $(window).lazyLoad('reload');
+        });
+    });
 });
