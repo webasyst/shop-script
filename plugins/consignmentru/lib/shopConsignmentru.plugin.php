b3d5f8754a80b97e009681ade6374bdff04c2697
Alexander Muzychenko <alexmuz@gmail.com>
2015-07-14 16:55:23 +0300

    plugin consignmentru

--- plugins/consignmentru/lib/shopConsignmentru.plugin.php
+++ plugins/consignmentru/lib/shopConsignmentru.plugin.php
@@ -1,66 +1,14 @@
 <?php
 
-class shopConsignmentruPlugin extends shopPlugin
+class shopConsignmentruPlugin extends shopPrintformPlugin
 {
-    private $templatepaths = array();
-    
-    private function getTemplatePaths()
+    public function allowedCurrency()
     {
-        if (!$this->templatepaths) {
-            $this->templatepaths = array(
-                'changed' => wa()->getDataPath('plugins/'.$this->id.'/template.html'),
-                'original' => $this->path . '/templates/actions/printform/PrintformDisplay.html'
-            );
-        }
-        return $this->templatepaths;
+        return array('RUB', 'UAH');
     }
-    
-    public function getTemplatePath()
+
+    protected function prepareForm(waOrder &$order, waView &$view)
     {
-        foreach ($this->getTemplatePaths() as $filepath) {
-            if (file_exists($filepath)) {
-                return $filepath;
-            }
-        }
-        return '';
+        $view->assign('items', $order->items);
     }
-    
-    public function getTemplate()
-    {
-        foreach ($this->getTemplatePaths() as $filepath) {
-            if (file_exists($filepath)) {
-                return file_get_contents($filepath);
-            }
-        }
-        return '';
-    }
-    
-    public function isTemplateChanged()
-    {
-        $paths = $this->getTemplatePaths();
-        return file_exists($paths['changed']);
-    }
-    
-    public function resetTemplate()
-    {
-        $paths = $this->getTemplatePaths();
-        $dir = dirname($paths['changed']);
-        waFiles::delete($paths['changed']);
-        waFiles::delete($dir.'/css/printform.css');
-        waFiles::delete($dir.'/js/printform.js');
-    }
-    
-    public function saveTemplate($data)
-    {
-        $paths = $this->getTemplatePaths();
-        file_put_contents($paths['changed'], $data);
-        waFiles::create(dirname($paths['changed']).'/css/');
-        waFiles::create(dirname($paths['changed']).'/js/');
-        file_put_contents(dirname($paths['changed']).'/css/printform.css', 
-            file_get_contents(dirname($paths['original']).'/css/printform.css')
-        );
-        file_put_contents(dirname($paths['changed']).'/js/printform.js', 
-            file_get_contents(dirname($paths['original']).'/js/printform.js')
-        );
-    }    
 }
