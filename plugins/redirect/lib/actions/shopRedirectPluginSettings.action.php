9fd5d6c5a01022ace76000ca6c8b324165919833
Alexander Muzychenko <alexmuz@gmail.com>
2015-09-28 17:51:06 +0300

    fix plugin redirect

--- plugins/redirect/lib/actions/shopRedirectPluginSettings.action.php
+++ plugins/redirect/lib/actions/shopRedirectPluginSettings.action.php
@@ -1,11 +1,36 @@
 <?php
 
-class shopRedirectPluginSettingsAction extends shopPluginsSettingsAction
+class shopRedirectPluginSettingsAction extends waViewAction
 {
 
     public function execute()
     {
-        $_GET['id'] = 'redirect';
-        parent::execute();
+
+        $plugin_id = 'redirect';
+        $vars = array();
+
+        /**
+         * @var shopPlugin $plugin
+         */
+        $plugin = waSystem::getInstance()->getPlugin($plugin_id, true);
+        $namespace = wa()->getApp().'_'.$plugin_id;
+
+        $params = array();
+        $params['id'] = $plugin_id;
+        $params['namespace'] = $namespace;
+        $params['title_wrapper'] = '%s';
+        $params['description_wrapper'] = '<br><span class="hint">%s</span>';
+        $params['control_wrapper'] = '<div class="name">%s</div><div class="value">%s %s</div>';
+
+        $settings_controls = $plugin->getControls($params);
+        $this->getResponse()->setTitle(_w(sprintf('Plugin %s settings', $plugin->getName())));
+
+        $vars['plugin_info'] = array(
+            'name' => $plugin->getName()
+        );
+        $vars['plugin_id'] = $plugin_id;
+        $vars['settings_controls'] = $settings_controls;
+
+        $this->view->assign($vars);
     }
 }
