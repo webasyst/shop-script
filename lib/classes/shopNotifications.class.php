56e4324d8a7ec7eefe99eb0b2ce0a99748655b40
woody-ru <woody@woody.ru>
2016-03-27 12:30:15 +0300

    Use template in from field in shop notifications.

--- lib/classes/shopNotifications.class.php
+++ lib/classes/shopNotifications.class.php
@@ -192,6 +192,7 @@ class shopNotifications
         $view->assign($data);
         $subject = $view->fetch('string:'.$n['subject']);
         $body = $view->fetch('string:'.$n['body']);
+        $from = $view->fetch('string:'.$from); //Use Smarty in from address
 
         $message = new waMailMessage($subject, $body);
         $message->setTo($to);
