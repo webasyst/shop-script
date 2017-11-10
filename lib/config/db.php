63d3c3aae7c35ae73b61a21e26700f38980f15ff
Leonix <leonix@motd.ru>
2017-11-10 16:04:37 +0300

    Shop v.7.2.20.174: fixed DB structure.

--- lib/config/db.php
+++ lib/config/db.php
@@ -403,8 +403,8 @@ return array(
         'virtualstock_id' => array('int', 11),
         'purchase_price' => array('decimal', "15,4", 'null' => 0, 'default' => '0.0000'),
         'total_discount' => array('decimal', "15,4", 'null' => 0, 'default' => '0.0000'),
-        'tax_percent' => array('decimal', "7,4", 'null' => 1, 'default' => 'null'),
-        'tax_included' => array('int', "1",'null' => 1, 'default' => '0'),
+        'tax_percent' => array('decimal', "7,4"),
+        'tax_included' => array('int', 1, 'null' => 0, 'default' => '0'),
         ':keys' => array(
             'PRIMARY' => 'id',
             'order_type' => array('order_id', 'type'),
@@ -482,6 +482,7 @@ return array(
         'logo' => array('text', 'null' => 0),
         'status' => array('int', 11, 'null' => 0),
         'sort' => array('int', 11, 'null' => 0),
+        'options' => array('text'),
         ':keys' => array(
             'PRIMARY' => 'id',
             'type' => 'type',
--- lib/updates/7.2/1510317175.php
+++ lib/updates/7.2/1510317175.php
@@ -9,8 +9,8 @@ try {
 }
 try {
     $m->query("SELECT `tax_included` FROM `shop_order_items` WHERE 0");
-    $m->query("UPDATE `shop_order_items` SET `tax_included`=1 WHERE `tax_included`");
-    $m->query("ALTER TABLE `shop_order_items` MODIFY `tax_included` int(1) DEFAULT '0'");
+    $m->query("UPDATE `shop_order_items` SET `tax_included`=IF(`tax_included`, 1, 0)");
+    $m->query("ALTER TABLE `shop_order_items` MODIFY `tax_included` int(1) NOT NULL DEFAULT '0'");
 } catch (waDbException $e) {
     $sql = "ALTER TABLE `shop_order_items` ADD `tax_included` int(1) DEFAULT '0'";
     $m->exec($sql);
