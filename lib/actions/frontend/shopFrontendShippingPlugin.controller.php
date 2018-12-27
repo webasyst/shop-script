b7bd32dbd14b16c05fb8a10bbfaff52b25b2f2b5
Anton <antonfedonjuk@gmail.com>
2018-12-27 04:02:54 +0300

    Update shopFrontendShippingPlugin.controller.php
    Позволяет вызывать экшены во время добавления плагина - http://site.ru/webasyst/shop/?action=settings#/shipping/plugin/add/myplugin/ (plugin_id передается имя плагина).

--- lib/actions/frontend/shopFrontendShippingPlugin.controller.php
+++ lib/actions/frontend/shopFrontendShippingPlugin.controller.php
@@ -4,13 +4,17 @@ class shopFrontendShippingPluginController extends waController
 {
     public function execute()
     {
-        $plugin_id = waRequest::param('plugin_id', 0, 'int');
+        $plugin_id = waRequest::param('plugin_id');
         if (!$plugin_id) {
             throw new waException('Plugin not found', 404);
         }
-
-        $plugin = shopShipping::getPlugin(null, $plugin_id);
-
+        
+        if (is_numeric($plugin_id)) {
+            $plugin = shopShipping::getPlugin(null, $plugin_id);
+        } else {
+            $plugin = shopShipping::getPlugin($plugin_id, null);
+        }
+        
         $action = waRequest::param('action_id');
         $method = $action.'Action';
         if (!$action || !method_exists($plugin, $method)) {
