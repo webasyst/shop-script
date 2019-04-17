b649fa1c784853e7b80746d912c0d7fd03bf2ebf
Leonix <leonix@motd.ru>
2019-04-17 17:02:37 +0300

    shop/plugins/consignmentru v.1.0.4   * Визуальное выделение всех редактируемых полей.   * Возможность редактирования размера налога для отдельных позиций,     чтобы устранить погрешность вычисления.   * Поддержка режима «Без НДС» для товаров без налога.

--- plugins/consignmentru/lib/config/plugin.php
+++ plugins/consignmentru/lib/config/plugin.php
@@ -5,7 +5,7 @@ return array(
     'description'    => 'Для использования на территории РФ.',
     'img'            => 'img/invoice16.png',
     'vendor'         => 'webasyst',
-    'version'        => '1.0.3',
+    'version'        => '1.0.4',
     'printform'      => true,
     'emailprintform' => false,
     'handlers'       => array(),
