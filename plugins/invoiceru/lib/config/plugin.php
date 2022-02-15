b2004ca68d7ba5eaf6b4f8042ffc5a9cae2734a9
Leonix <leonix@motd.ru>
2022-02-15 14:34:58 +0300

    shop/plugins/invoiceru v.1.1.5   * Добавили поддержку дробного количества и единиц измерения количества товаров.

--- plugins/invoiceru/lib/config/plugin.php
+++ plugins/invoiceru/lib/config/plugin.php
@@ -5,7 +5,7 @@ return array(
     'description' => 'Для использования на территории РФ.',
     'img'         => 'img/invoice16.png',
     'vendor'      => 'webasyst',
-    'version'     => '1.1.4',
+    'version'     => '1.1.5',
     'printform'   => true,
     'emailprintform' => false,
     'handlers'    => array(),
