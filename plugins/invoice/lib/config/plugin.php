324da9414c6aad28172e60205427d81755ba0f23
Leonid Vakulenko <leonix@motd.ru>
2025-02-12 13:18:09 +0300

    plugins/invoice v.1.0.3   * Fixed the calculation of taxes in the printable form.

--- plugins/invoice/lib/config/plugin.php
+++ plugins/invoice/lib/config/plugin.php
@@ -3,7 +3,7 @@ return array(
     'name'           => /*_wp*/'Invoice',
     'description'    => /*_wp*/'Print International Invoice form.',
     'img'           => 'img/invoice16.png',
-    'version'        => '1.0.2',
+    'version'        => '1.0.3',
     'vendor'         => 'webasyst',
     'printform'      => true,
     'emailprintform' => false,
